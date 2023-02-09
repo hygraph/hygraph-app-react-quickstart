db0417545c835ea5bc4b0c9e0520fc9b9faa38b1
Bryan Robinson <bryanlrobinson@gmail.com>
2023-02-09 12:04:38 -0500

    Initial router commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Field from './components/field';
+import Page from './components/page';
+import Setup from './components/setup';
+import Sidebar from './components/sidebar';
+
+import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route exact path="setup" element={<Setup />} />
+        <Route exact path="page" element={<Page />} />
+        <Route exact path="field" element={<Field />} />
+        <Route exact path="sidebar" element={<Sidebar />} />
+        
+      </Routes>
+    </Router>
+  </React.StrictMode>
+
+);
